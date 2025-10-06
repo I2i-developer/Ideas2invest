25f911ecd870e03582c4dab530c2b2f81a283f08
I2i-developer <developerhead.i2i@gmail.com>
2025-10-06 12:21:19 +0530

    New Commits

--- src/components/GoogleAnalytics/GoogleAnalyticsTracker.js
+++ src/components/GoogleAnalytics/GoogleAnalyticsTracker.js
@@ -1,18 +1,27 @@
 'use client'
 import { usePathname, useSearchParams } from 'next/navigation'
-import { useEffect } from 'react'
+import { useEffect, Suspense } from 'react'
 
-export default function GoogleAnalyticsTracker() {
+function Tracker() {
   const pathname = usePathname()
   const searchParams = useSearchParams()
 
   useEffect(() => {
     if (!pathname) return
-    const url = pathname + searchParams.toString()
+    const url = `${pathname}?${searchParams.toString()}`
     window.gtag?.('config', 'G-TTWMS72K7P', {
       page_path: url,
     })
   }, [pathname, searchParams])
 
   return null
+}
+
+export default function GoogleAnalyticsTracker() {
+  // ✅ Wrap inside Suspense boundary
+  return (
+    <Suspense fallback={null}>
+      <Tracker />
+    </Suspense>
+  )
 }
